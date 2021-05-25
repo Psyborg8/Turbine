b6a1f56fa37ec8ad0134cf18fdfd3f501a50346a
Jon Arscott <headlesshero@hotmail.co.uk>
2021-05-25 16:49:58 +0200

    Adding some tilesets and started using Tiled for level editing

---
+++ Assets/Tilesets/Stronghold_Terrain_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Stronghold" tilewidth="16" tileheight="16" tilecount="98" columns="14">
+ <image source="Multi_Platformer_Tileset_v2/Stronghold/Terrain/Stronghold_Terrain_Tileset.png" width="224" height="112"/>
+</tileset>
